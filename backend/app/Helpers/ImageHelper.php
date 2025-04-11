1c1a6cff7a240a117719a0960f1bcd293db33af8
khaipt04 <khaipt04@gmail.com>
2025-04-11 20:56:23 +0700

    create helper handle upload image

--- backend/app/Helpers/ImageHelper.php
+++ backend/app/Helpers/ImageHelper.php
@@ -7,6 +7,30 @@ use Intervention\Image\Drivers\Imagick\Driver;
 
 class ImageHelper
 {
+    public static function handleUpload($file, $folder, $prefix, $resizeType)
+    {
+        $image = null;
+
+        if ($resizeType === 'square'){
+            $image = ImageHelper::resizeToSquare($file);
+        } elseif ($resizeType === 'rectangle'){
+            $image = ImageHelper::resizeToRectangle($file);
+        }
+
+        $extension = $file->getClientOriginalExtension();
+        $imageName = $prefix . now()->format('Ymd_His') . '_' . uniqid() . '.' . $extension;
+
+        $relativePath = 'uploads/' . $folder . '/' . $imageName;
+        $fullPath = storage_path('app/public/' . $relativePath);
+
+        $image->save($fullPath);
+
+        return [
+            'image_path' => $relativePath,
+            'image_url' => asset('storage/' . $relativePath)
+        ];
+    }
+
     public static function resizeToSquare($file)
     {
         $manager = new ImageManager(new Driver());
@@ -21,6 +45,24 @@ class ImageHelper
 
         if ($image->width() > $width || $image->height() > $height) {
             $image->resize($width, $height);
+        }
+
+        return $image;
+    }
+
+    public static function resizeToRectangle($file)
+    {
+        $manager = new ImageManager(new Driver());
+
+        $image = $manager->read($file);
+
+        $targetWidth = 1920;
+        $targetHeight = intval($targetWidth * 9 / 16);
+
+        $image->cover($targetWidth, $targetHeight);
+
+        if ($image->width() > $targetWidth || $image->height() > $targetHeight) {
+            $image->resize($targetWidth, $targetHeight);
         }
 
         return $image;
